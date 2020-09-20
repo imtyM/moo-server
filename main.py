285a376ed1977a4c559d6c7cdd616f847ebf5a46
Mohamed Ameen Omar <amzomra@amazon.com>
2020-09-20 19:31:00 +0200

    Removed fun prints.

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ print('starting process')
 server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 server_sock.bind(("", bluetooth.PORT_ANY))
 server_sock.listen(1)
-print('Things are working?')
 port = server_sock.getsockname()[1]
 
 uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
