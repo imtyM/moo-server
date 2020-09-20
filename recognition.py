c788a806475eb6473dfd98e79de5c3b7161cb9cd
imtyM <m.imtiazmukadam@gmail.com>
2020-09-20 20:47:23 +0200

    Shit code

--- recognition.py
+++ recognition.py
@@ -1,7 +1,9 @@
 import cv2 as cv
 
-cap = cv.VideoCapture('./cow_data/WIN_20200907_12_29_56_Pro.mp4')
+__input = './cow_data/WIN_20200907_12_29_56_Pro.mp4'
+cap = cv.VideoCapture(__input)
 
+frame_counter = 0
 if cap.isOpened() == False:
     print('Could not open file')
 while True:
@@ -10,4 +12,7 @@ while True:
         print('no frame left')
         break
     else:
+        frame_counter = frame_counter + 1
+        print('Showing frame ', frame_counter)
         cv.imshow('', frame)
+        cv.waitKey(0)
