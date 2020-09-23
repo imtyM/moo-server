00577bb69d1ea3bd66666fa8aaefdd1a412c1f8b
imtyM <m.imtiazmukadam@gmail.com>
2020-09-23 19:38:11 +0200

    Add: Working bluetooth_server

--- bluetooth_server.py
+++ bluetooth_server.py
@@ -1,19 +1,48 @@
-import bluetooth
+#!/usr/bin/env python
+"""
+A simple test server that returns a random number when sent the text "temp" via Bluetooth serial.
+"""
 
-server_sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+from bluetooth import *
 
-server_sock.bind(("",bluetooth.PORT_ANY))
+server_sock = BluetoothSocket( RFCOMM  )
+server_sock.bind(("",PORT_ANY))
 server_sock.listen(1)
 
-uuid = "1e0ca4ea-299d-4335-93eb-27fcfe7fa848"
-bluetooth.advertise_service( server_sock, "FooBar Service", uuid  )
+port = server_sock.getsockname()[1]
 
-print("Waiting for connections here...")
-client_sock,address = server_sock.accept()
-print("Accepted connection from ",address)
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-data = client_sock.recv(1024)
-print("received: ",data)
+advertise_service(
+        server_sock, "TestServer",
+        service_id = uuid,
+        service_classes = [ uuid, SERIAL_PORT_CLASS  ],
+        profiles = [ SERIAL_PORT_PROFILE  ],
+        #protocols = [ OBEX_UUID  ]
+)
 
-client_sock.close()
-server_sock.close()
+print("Waiting for connection on RFCOMM channel ", port)
+client_sock, client_info = server_sock.accept()
+print ("Accepted connection from ", client_info)
+
+while True:
+    try:
+        req = client_sock.recv(1024)
+        if len(req) == 0:
+            break
+        print("received ", req)
+
+        print("sending ", req)
+        client_sock.send(req)
+
+    except IOError:
+        pass
+
+    except KeyboardInterrupt:
+        print("disconnected")
+
+        client_sock.close()
+        server_sock.close()
+        print("all done")
+
+        break
