c395e08b3dbb64dde5859803592413b879e626ba
imtyM <m.imtiazmukadam@gmail.com>
2020-10-22 23:11:55 +0200

    update

--- blue.py
+++ blue.py
@@ -55,8 +55,8 @@ class Blue:
     # @return data_recieved [String]: recieved data
     def processInputFromBluetooth(self, mode):
         data_recieved = self.recieve_data()
-        recieved_mode = Int(data_recieved)
-        if not type(recieved_mode) == Int or recieved_mode < 0 or recieved_mode > 2:
+        recieved_mode = int(data_recieved)
+        if not type(recieved_mode) == int or recieved_mode < 0 or recieved_mode > 2:
             raise('The recieved_mode needs to be an int, got this tho: ', data_recieved)
         return Modes(recieved_mode), data_recieved
 
