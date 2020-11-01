e1bef0afa002d77e5506893ae9fec9ab7b91669a
imtyM <m.imtiazmukadam@gmail.com>
2020-11-01 17:11:44 +0200

    update

--- main.py
+++ main.py
@@ -11,39 +11,39 @@ bluetooth = Blue()
 weight_detector = WeightDetector(debug=False)
 
 while True:
-    try:
-        references = weight_detector.get_sensor_references()
-        print('Mode: ', mode)
+    # try:
+    references = weight_detector.get_sensor_references()
+    print('Mode: ', mode)
 
-        data_recieved, mode, references, tare, should_send_next_frame = bluetooth.processInputFromBluetooth(mode, references)
-        if data_recieved:
-            print('Recieved data: ', data_recieved)
-            # bluetooth.processOutputToBluetooth(True, data_recieved, 1)
-            weight_detector.set_sensor_references(references)
-            weight_detector.tare(tare)
+    data_recieved, mode, references, tare, should_send_next_frame = bluetooth.processInputFromBluetooth(mode, references)
+    if data_recieved:
+        print('Recieved data: ', data_recieved)
+        # bluetooth.processOutputToBluetooth(True, data_recieved, 1)
+        weight_detector.set_sensor_references(references)
+        weight_detector.tare(tare)
 
-            # Send next frame if requested by client
-            base_64_image = image_processor.get_next_frame_base_64(should_send_next_frame)
-            bluetooth.send_next_frame(should_send_next_frame)
-            continue
+        # Send next frame if requested by client
+        base_64_image = image_processor.get_next_frame_base_64(should_send_next_frame)
+        bluetooth.send_next_frame(should_send_next_frame)
+        continue
 
-        if mode == Modes.DETECT:
-            valid, cow_data = weight_detector.detectCowLameness()
-            cow_id = image_processor.detectCow(valid)
-            bluetooth.send_payload(
-                valid=valid,
-                cow_data=cow_data,
-                cow_id=cow_id,
-                mode=mode,
-                references=references
-            )
+    if mode == Modes.DETECT:
+        valid, cow_data = weight_detector.detectCowLameness()
+        cow_id = image_processor.detectCow(valid)
+        bluetooth.send_payload(
+            valid=valid,
+            cow_data=cow_data,
+            cow_id=cow_id,
+            mode=mode,
+            references=references
+        )
 
-        if mode == Modes.IDLE:
-            bluetooth.send_payload(mode=mode, references=references)
+    if mode == Modes.IDLE:
+        bluetooth.send_payload(mode=mode, references=references)
 
-        time.sleep(0.5)
-    except:
-       bluetooth.setupBluetoothProcessing()
+    time.sleep(0.5)
+    # except:
+       # bluetooth.setupBluetoothProcessing()
 
     # if mode == Modes.REGISTER:
         # # TODO: Lameness detector doesnt exist
